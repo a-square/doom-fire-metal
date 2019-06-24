efa5765dfb2fddcf59b73c4274db4293c718bd84
Alexei Averchenko <lex.aver@gmail.com>
2019-06-24 23:08:55 +0300

    Removed correlation between direction and energy

--- DoomFire/FireBuffer.c
+++ DoomFire/FireBuffer.c
@@ -37,12 +37,15 @@ void destroyFireBuffer(int32_t* restrict buffer) {
 }
 
 void updateFireBuffer(int32_t* restrict buffer, uint32_t width, uint32_t height) {
-    uint32_t rnd = (rand() << 16) ^ (rand() & 0xffffu);
+    uint32_t rnd1 = (rand() << 16) ^ (rand() & 0xffffu);
+    uint32_t rnd2 = (rand() << 16) ^ (rand() & 0xffffu);
     uint32_t effectiveHeight = height - (height >> 2);
     for (uint32_t y = 0; y < effectiveHeight; ++y) {
         for (uint32_t x = 0; x < width; x += 16) {
-            rnd = xorshift32(rnd);
-            uint32_t rndRem = rnd;
+            rnd1 = xorshift32(rnd1);
+            rnd2 = xorshift32(rnd2);
+            uint32_t rem1 = rnd1;
+            uint32_t rem2 = rnd2;
             uint32_t base = y * width + x;
             for (uint32_t i = 0; i < 16; i += 4) {
                 uint32_t src = base + i;
@@ -54,22 +57,28 @@ void updateFireBuffer(int32_t* restrict buffer, uint32_t width, uint32_t height)
 
                 uint32_t dstBase = src + width + 2;
 
-                uint32_t rem0 = rndRem;
-                uint32_t rem1 = rndRem >> 2;
-                uint32_t rem2 = rndRem >> 4;
-                uint32_t rem3 = rndRem >> 6;
+                uint32_t rem10 = rem1;
+                uint32_t rem11 = rem1 >> 2;
+                uint32_t rem12 = rem1 >> 4;
+                uint32_t rem13 = rem1 >> 6;
 
-                uint32_t dst0 = dstBase - (rem0 & 3);
-                uint32_t dst1 = dstBase + 1 - (rem1 & 3);
-                uint32_t dst2 = dstBase + 2 - (rem2 & 3);
-                uint32_t dst3 = dstBase + 3 - (rem3 & 3);
+                uint32_t rem20 = rem2;
+                uint32_t rem21 = rem2 >> 1;
+                uint32_t rem22 = rem2 >> 2;
+                uint32_t rem23 = rem2 >> 3;
 
-                buffer[dst0] = p0 - (rem0 & 1);
-                buffer[dst1] = p1 - (rem1 & 1);
-                buffer[dst2] = p2 - (rem2 & 1);
-                buffer[dst3] = p3 - (rem3 & 1);
+                uint32_t dst0 = dstBase - (rem10 & 3);
+                uint32_t dst1 = dstBase + 1 - (rem11 & 3);
+                uint32_t dst2 = dstBase + 2 - (rem12 & 3);
+                uint32_t dst3 = dstBase + 3 - (rem13 & 3);
 
-                rndRem >>= 8;
+                buffer[dst0] = p0 - (rem20 & 1);
+                buffer[dst1] = p1 - (rem21 & 1);
+                buffer[dst2] = p2 - (rem22 & 1);
+                buffer[dst3] = p3 - (rem23 & 1);
+
+                rem1 >>= 8;
+                rem2 >>= 4;
             }
         }
     }
